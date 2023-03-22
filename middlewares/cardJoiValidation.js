ee1e96986d9bdb887c640c615d9dde62806abe4e
ArtemiiKokodeev <artemkokodeev@gmail.com>
2023-03-22 22:01:12 +0300

    13. Some fix in joi middlewares

--- middlewares/cardJoiValidation.js
+++ middlewares/cardJoiValidation.js
@@ -3,17 +3,17 @@ const { celebrate, Joi } = require('celebrate');
 // проверка url картинки через регулярное выражение:
 const regex = /(ftp|http|https):\/\/.(www\.)?[a-z\-\d]+\.[\w\-.~:/?#[\]@!$&'()*+,;=]{1,}#?/i;
 
-// проверка полей при создании юзера
+// проверка полей при создании карточки
 module.exports.createCardJoiValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().regex(regex),
+    link: Joi.string().required().regex(regex),
   }),
 });
 
 // проверка _id при удалении карточки, установке или снятии лайка
 module.exports.cardIdJoiValidation = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().min(24).max(24),
+    cardId: Joi.string().required().min(24).max(24),
   }),
 });
