bce5553aa8b73b1d2fe10f3e48289da09ca73792
ArtemiiKokodeev <artemkokodeev@gmail.com>
2023-03-23 21:53:43 +0300

    15. Simplyfied statusCode check in errorHandler

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,26 +1,16 @@
 const {
-  UNAUTHORIZED, BAD_REQUEST, FORBIDDEN, NOT_FOUND, NOT_UNIQUE, INTERNAL_SERVER_ERROR,
+  BAD_REQUEST, NOT_UNIQUE, INTERNAL_SERVER_ERROR,
 } = require('../utils/constants');
 
 const errorHandler = (err, req, res, next) => {
-  if (err.code === 11000) {
+  if (err.statusCode) {
+    res.status(err.statusCode).send(
+      { message: err.message },
+    );
+    return;
+  } if (err.code === 11000) {
     res.status(NOT_UNIQUE).send(
       { message: `Пользователь с такими данными уже существует: ${err.message}` },
-    );
-    return;
-  } if (err.statusCode === UNAUTHORIZED) {
-    res.status(err.statusCode).send(
-      { message: err.message },
-    );
-    return;
-  } if (err.statusCode === NOT_FOUND) {
-    res.status(err.statusCode).send(
-      { message: err.message },
-    );
-    return;
-  } if (err.statusCode === BAD_REQUEST) {
-    res.status(err.statusCode).send(
-      { message: err.message },
     );
     return;
   } if (err.name === 'ValidationError') {
@@ -31,11 +21,6 @@ const errorHandler = (err, req, res, next) => {
   } if (err.name === 'CastError') {
     res.status(BAD_REQUEST).send(
       { message: `Переданы некорректные данные: ${err.message}` },
-    );
-    return;
-  } if (err.statusCode === FORBIDDEN) {
-    res.status(err.statusCode).send(
-      { message: err.message },
     );
     return;
   }
